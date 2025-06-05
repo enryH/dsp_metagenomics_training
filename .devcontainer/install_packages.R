730878d69e3761de5cb1696225577e9a2cb117b8
Albert Palleja Caro <apca@biosustain.dtu.dk>
2025-06-05 10:40:56 +0200

    Added netcomi R libraries dependencies, ggrepel and python-venv

--- .devcontainer/install_packages.R
+++ .devcontainer/install_packages.R
@@ -1,8 +1,17 @@
 install.packages(
   c(
-    "FSA", "languageserver", "netcomi"
+    "FSA", "languageserver", "BiocManager"
   ),
   dependencies = TRUE,
   repos = "https://cloud.r-project.org",
   Ncpus = parallel::detectCores()
 )
+
+# Install NetCoMi dependencies
+devtools::install_github("zdk123/SpiecEasi")
+devtools::install_github("GraceYoon/SPRING")
+
+# Install NetCoMi
+devtools::install_github("stefpeschel/NetCoMi",
+                         repos = c("https://cloud.r-project.org/",
+                                   BiocManager::repositories()))
